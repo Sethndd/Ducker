72a82efb0f68be361579787c98a30ab9f17b1fdf
DrearyStudent <Jeff_Withe@hotmail.com>
2021-11-27 14:00:13 -0600

    Creación de rutas.

---
+++ api/src/dataAccess/likesDAO.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const dbConnection = require(path.join(__dirname, 'dbConnection.js'))
+
+function crearLikes(idQuack, idUsuario, callback){
+    dbConnection.query('call crearLike(?, ?)', [idQuack, idUsuario], (err, rows, fields) =>{
+        if(err){
+            return callback(err)
+        }
+        else{
+            callback(null, rows)
+        }
+    })
+}
+
+function obtenerCantidadLikesQuack(idQuack, callback){
+    dbConnection.query('call obtenerCantidadLikesQuack(?)', [idQuack], (err, rows, fields)=>{
+        if (err){
+            return callback(err)
+        }
+        else {
+            callback(null, rows)
+        }
+    })
+}
+
+function eliminarLikes(id, callback){
+    dbConnection.query('call eliminarLike(?)', [id] , (err, rows, fields) =>{
+        if(err){
+            return callback(err)
+        }
+        else{
+            callback(null, rows[0])
+        }
+    })
+}
+
+module.exports = {crearLikes, obtenerCantidadLikesQuack, eliminarLikes}
